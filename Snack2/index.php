09a685d16d330a462b50c626a6c6c8797b73b4e6
MattiaLongo03 <mattilongo2003@gmail.com>
2022-12-14 16:59:27 +0100

    Snack 2 completato

--- Snack2/index.php
+++ Snack2/index.php
@@ -7,6 +7,34 @@
     <title>Snack 2</title>
 </head>
 <body>
-    
+<?php 
+    $name = isset($_GET['name']) ? $_GET['name'] : '';
+    $mail = isset($_GET['mail']) ? $_GET['mail'] : '';
+    $age = isset($_GET['age']) ? $_GET['age'] : '';
+
+    $pre = strpos($mail, '@');
+    $post = strpos($mail, '.', $pre);
+
+    if (strlen(trim($name)) > 3 && $pre && $post && is_numeric($age)) {
+        $accesso = 'Accesso riuscito';
+    } 
+    else {
+        $accesso = 'Accesso negato';
+    }
+    ?>
+
+    <form action="" method="get">
+            <label for="name">Nome</label>
+            <input type="text" name="name" id="name">
+
+            <label for="mail">Email</label>
+            <input type="mail" name="mail" id="mail">
+
+            <label for="age">Età</label>
+            <input type="text" name="age" id="age">
+
+            <button>Controlla</button>
+        </form>
+    <h2><?= $accesso ?></h2>
 </body>
 </html>
